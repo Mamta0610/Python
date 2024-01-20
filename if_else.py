e6d304fab088a0638106237609d87beaaa3f834c
Mamta0610 <106583262+Mamta0610@users.noreply.github.com>
2024-01-20 14:16:04 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,54 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=int(input(“Enter First number:”))
+SyntaxError: invalid character '“' (U+201C)
+x=int(input("Enter First number:"))
+Enter First number:30
+y=int(input("Enter First number:"))
+Enter First number:20
+if(x>y):
+    print(x, " is greater ")
+    else:
+        
+SyntaxError: invalid syntax
+>>> if(x>y):
+...     print(x, " is greater")
+... else:
+...     print(y,  " is greater")
+... 
+30  is greater
+>>> 
+>>> 
+>>> per=float(input(“Enter Percentage”))
+SyntaxError: invalid character '“' (U+201C)
+>>> per=float(input("Enter Percentage"))
+Enter Percentage 65
+>>> if(per>=65)"
+SyntaxError: unterminated string literal (detected at line 1)
+>>> if(per>=65):
+...     print("A grade ")
+... else:
+...     if per>=60:
+...         print(" B grade")
+...     else:
+...         if per>=50:
+...             print("C grade")
+...     else:
+...         
+SyntaxError: invalid syntax
+>>> per=float(input(“Enter Percentage :  ”))
+SyntaxError: invalid character '“' (U+201C)
+>>> per=float(input("Enter Percentage  :  "))
+Enter Percentage  :  75
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> if(per>=65):
+...     print("A grade ")
+... else:
+...     if per>=60
+...     
+SyntaxError: expected ':'
---
+++ nestedif.py
@@ -0,0 +1,26 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Display Grades of the student as per the percentage entered by Student.
+... per=float(input(" Enter Percentage : "))
+ Enter Percentage : 55
+>>> if per>=80:
+...     print(" O grade ")
+... else:
+...     if per>=70
+...     
+SyntaxError: expected ':'
+>>> if per>=70:
+...     print("A grade ")
+... else:
+...     if per>=60:
+...         print("B grade ")
+...     else:
+...         if per>=50:
+...             print("C grade ")
+...         else:
+...             if per>=40:
+...                 print("D grade ")
+...             else:
+...                 print("sorry failed")
+... 
+C grade 

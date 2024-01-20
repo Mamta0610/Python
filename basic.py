d25d6d54f700d735ef79ec1434a1204b4c6ee180
Mamta0610 <106583262+Mamta0610@users.noreply.github.com>
2024-01-20 12:26:01 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,24 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> print("My first python program")
+My first python program
+>>> print("i am learning python ")
+i am learning python 
+>>> print("Hello, Good Morning ")
+Hello, Good Morning 
+>>> message = 'python is fun to learn '
+>>> print(message)
+python is fun to learn 
+>>> print('python', 'is', 'fun')
+python is fun
+>>> Print ('Python','is','fun', sep='\n')
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    Print ('Python','is','fun', sep='\n')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print('Python','is','fun', sep='\n')
+Python
+is
+fun
